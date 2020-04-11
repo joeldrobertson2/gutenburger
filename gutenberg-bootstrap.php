95cf49f24419a043607bfd5d1e3377455efe9bb3
Joel Robertson <joelrobertson@Joels-MacBook-Pro.local>
2020-04-11 17:04:17 +1000

    Initial commit

---
+++ gutenberg-bootstrap.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Gutenburgers.
+ *
+ * @package     joelsGutenBurgers
+ * @author      Joel Robertson
+ * @license     GPL2+
+ *
+ * @wordpress-plugin
+ * Plugin Name: Gutenburgers
+ * Description: Some test blocks for fun and profit
+ * Version:     1.0
+ * Author:      Joel Robertson
+ * License:     GPL2+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+ 
